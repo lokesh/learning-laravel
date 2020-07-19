30fee417315270351237c2df43673a2772dc8e29
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2020-07-19 15:07:43 -0700

    controller refactoring

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,75 +9,87 @@ use App\Post;
 
 class PostController extends Controller
 {
-        public function index()
-        {
-            return view('posts.index', [
-                'posts' => Post::latest()->get()
-            ]);
-        }
+    public function index()
+    {
+        return view('posts.index', [
+            'posts' => Post::latest()->get()
+        ]);
+    }
 
-        public function show(Post $post)
-        {
-            return view('posts.show', ['post' => $post]);
-        }
+    public function show(Post $post)
+    {
+        return view('posts.show', ['post' => $post]);
+    }
 
-        /**
-         * Show a view to create a new resource
-         */
-        public function create()
-        {
-            return view('posts.create');
-        }
+    /**
+     * Show a view to create a new resource
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
 
-        /**
-         * Persist the new resource
-         */
-        public function store()
-        {
-            request()->validate([
-                'title' => ['required', 'min:3'],
-                'body' => 'required'
-            ]);
+    /**
+     * Persist the new resource
+     */
+    public function store()
+    {
+        $this->validatePost();
 
-            $post = new Post();
+        // Refactor of code below
+        Post::create([
+            'title' => request('title'),
+            'body' => request('body'),
+            'slug' => Str::slug(request('title'), '-')
+        ]);
 
-            $post->title = request('title');
-            $post->body = request('body');
-            $post->slug = Str::slug(request('title'), '-');
+        // $post = new Post();
+        // $post->title = request('title');
+        // $post->body = request('body');
+        // $post->slug = Str::slug(request('title'), '-');
+        // $post->save();
 
-            $post->save();
+        return redirect('/posts');
+    }
 
-            return redirect('/posts');
-        }
+    /**
+     * Show a view to edit a resource
+     */
+    public function edit(Post $post) {
+        return view('posts.edit', ['post' => $post]);
+    }
 
-        /**
-         * Show a view to edit a resource
-         */
-        public function edit(Post $post) {
-            return view('posts.edit', ['post' => $post]);
-        }
+    /**
+     * Persist the edited resource
+     */
+    public function update(Post $post)
+    {
+        // Refactor of code below
+        $post->update($this->validatePost());
 
-        /**
-         * Persist the edited resource
-         */
-        public function update(Post $post)
-        {
+        // request()->validate([
+        //     'title' => ['required', 'min:3'],
+        //     'body' => 'required'
+        // ]);
 
-            request()->validate([
-                'title' => ['required', 'min:3'],
-                'body' => 'required'
-            ]);
+        // $post->title = request('title');
+        // $post->body = request('body');
+        // $post->save();
 
-            $post->title = request('title');
-            $post->body = request('body');
-            $post->save();
+        return redirect('/posts/' . $post->slug);
+    }
 
-            return redirect('/posts/' . $post->slug);
-        }
+    public function destroy(Post $post) {
+        $post->delete();
 
-        public function destroy(Post $post) {
-            $post->delete();
+        return redirect('/posts/');
+    }
 
-            return redirect('/posts/');
-        }
+    protected function validatePost()
+    {
+        return request()->validate([
+            'title' => ['required', 'min:3'],
+            'body' => 'required'
+        ]);
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+
+    /*
+    Avoid using request params in mass to generate an item. Someone could append
+    additional data and override fields that shouldn't be set.
+
+    E.g.
+    Post::create(request()->all())
+
+    If there is a form to update a customer's username, they could pass
+    additional data such as their subscribtion status.
+    [ 'username' => 'newname', 'subscriber' => true]
+
+    To prevent this, specific the specific fields that are whitelisted for usage
+    with the create() method. Or, disable the security feature by setting
+    $guarded to an empty array and avoid using request()->all()
+     */
+
+
+    // protected $fillable = ['title', 'body', 'slug'];
+    protected $guarded = [];
+
     /**
      * We're overwriting this method as we're using the slug, not the id, from
      * the Post db table for our routes.
